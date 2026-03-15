c75dcd80927ac3890bbe4378cae69cfd55303fc4
Xr4t0s <nicolastadros.maillol@gmail.com>
2026-03-15 18:33:55 +0100

    update

--- install.sh
+++ install.sh
@@ -7,22 +7,23 @@ set -euo pipefail
 sudo apt-get update && sudo apt-get upgrade -y
 
 # Installing docker
-sudo apt install docker.io \
-	ca-certificates curl
+sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-Types: deb
-URIs: https://download.docker.com/linux/debian
-Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
-Components: stable
-Signed-By: /etc/apt/keyrings/docker.asc
+	Types: deb
+	URIs: https://download.docker.com/linux/debian
+	Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+	Components: stable
+	Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
 sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Adding hostnames
 echo "127.0.0.1 nitadros.42.fr" | sudo tee -a /etc/hosts
