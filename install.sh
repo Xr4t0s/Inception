770a5d845b598fd6c26f6730e8195a2572ed8db1
Xr4t0s <nicolastadros.maillol@gmail.com>
2026-03-15 20:16:53 +0100

    update

--- install.sh
+++ install.sh
@@ -17,7 +17,6 @@ if ! groups | grep docker; then
 fi
 if ! groups "$USER" | grep docker; then
 	sudo usermod -aG docker $USER
-	newgrp docker
 fi
 
 # Adding hostnames
@@ -25,7 +24,7 @@ echo "127.0.0.1 nitadros.42.fr" | sudo tee -a /etc/hosts
 
 sudo docker compose up -d --build
 
-CAROOT=$(docker exec "$CONTAINER" mkcert -CAROOT)
+CAROOT=$(docker exec nginx mkcert -CAROOT)
 sudo docker cp "nginx:$CAROOT/rootCA.pem" .
 sudo cp rootCA.pem /usr/local/share/ca-certificates/mkcert.crt
 sudo update-ca-certificates
