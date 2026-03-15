341cf07cf6f446720d82faef18187b571c24ae16
Xr4t0s <nicolastadros.maillol@gmail.com>
2026-03-15 19:11:23 +0100

    corected docker installation on the host virtual machine

--- install.sh
+++ install.sh
@@ -6,25 +6,13 @@ set -euo pipefail
 # Update environnement
 sudo apt-get update && sudo apt-get upgrade -y
 
-# Installing docker
-sudo apt-get install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt install snapd
 
-# Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-	Types: deb
-	URIs: https://download.docker.com/linux/debian
-	Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
-	Components: stable
-	Signed-By: /etc/apt/keyrings/docker.asc
-EOF
+sudo systemctl start snapd
 
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo snap install docker
 
 # Adding hostnames
 echo "127.0.0.1 nitadros.42.fr" | sudo tee -a /etc/hosts
- 
+
+sudo docker compose up -d --build
