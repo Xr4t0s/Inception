3fe2950500076b03150e98a2fd6c3b582527c714
Xr4t0s <nicolastadros.maillol@gmail.com>
2026-03-15 18:29:07 +0100

    update

--- install.sh
+++ install.sh
@@ -6,10 +6,23 @@ set -euo pipefail
 # Update environnement
 sudo apt-get update && sudo apt-get upgrade -y
 
-sudo apt-get install snap -y
-
 # Installing docker
-sudo snap install docker
+sudo apt install docker.io \
+	ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
 
 # Adding hostnames
 echo "127.0.0.1 nitadros.42.fr" | sudo tee -a /etc/hosts
