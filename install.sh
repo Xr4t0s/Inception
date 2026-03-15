137441a5cfd5dd32a356d0f283b09f5ece8e2bd4
Xr4t0s <nicolastadros.maillol@gmail.com>
2026-03-15 15:32:18 +0100

    impl: install and deploy containers

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+############ Configure host ############
+
+# Update environnement
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install snap -y
+
+# Installing docker
+sudo snap install docker
+
+echo "127.0.0.1 nitadros.42.fr" | sudo tee -a /etc/hosts
+ 
