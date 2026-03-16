5e360ce70b90619a46f27a83f2c65c1e7fc71436
Xr4t0s <nicolastadros.maillol@gmail.com>
2026-03-16 15:41:15 +0100

    update

--- requirements/wordpress/install-wordpress.sh
+++ requirements/wordpress/install-wordpress.sh
@@ -21,7 +21,7 @@ wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 php8.4-cli ./wp-cli.phar
 
-php /usr/local/bin/wp-cli.phar core install \
+php8.4-cli /usr/local/bin/wp-cli.phar core install \
   --url="https://${DOMAIN_NAME}" \
   --title="Inception" \
   --admin_user="${WP_ADMIN_USER}" \
@@ -29,7 +29,7 @@ php /usr/local/bin/wp-cli.phar core install \
   --admin_email="${WP_ADMIN_EMAIL}" \
   --allow-root
 
-php /usr/local/bin/wp-cli.phar user create "$WP_USER" "$WP_USER_EMAIL" \
+php8.4-cli /usr/local/bin/wp-cli.phar user create "$WP_USER" "$WP_USER_EMAIL" \
   --user_pass="$WP_USER_PASSWORD" \
   --path=/website \
 	--allow-root
