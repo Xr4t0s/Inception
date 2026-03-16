480832f63928f58c89fef5b4b2917a62b9d14b28
Xr4t0s <nicolastadros.maillol@gmail.com>
2026-03-16 15:58:31 +0100

    update

--- requirements/wordpress/install-wordpress.sh
+++ requirements/wordpress/install-wordpress.sh
@@ -8,28 +8,28 @@ apt-get update && apt-get upgrade -y && apt-get install -y \
 
 
 
-wget https://wordpress.org/latest.zip
-unzip ./latest.zip
+# wget https://wordpress.org/latest.zip
+# unzip ./latest.zip
 
-rm -rf /var/www/html/* /var/www/html/.*
-mv wordpress/* /var/www/html/
-chown -R www-data:www-data /var/www/html
+# rm -rf /var/www/html/* /var/www/html/.*
+# mv wordpress/* /var/www/html/
+# chown -R www-data:www-data /var/www/html
 
-sed -i 's|/run/php/php.*sock|9000|' /etc/php/*/fpm/pool.d/www.conf
+# sed -i 's|/run/php/php.*sock|9000|' /etc/php/*/fpm/pool.d/www.conf
 
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-php8.4-cli ./wp-cli.phar
+# php8.4-cli ./wp-cli.phar
 
-php8.4-cli /usr/local/bin/wp-cli.phar core install \
-  --url="https://${DOMAIN_NAME}" \
-  --title="Inception" \
-  --admin_user="${WP_ADMIN_USER}" \
-  --admin_password="${WP_ADMIN_PASSWORD}" \
-  --admin_email="${WP_ADMIN_EMAIL}" \
-  --allow-root
+# php8.4-cli /usr/local/bin/wp-cli.phar core install \
+#   --url="https://${DOMAIN_NAME}" \
+#   --title="Inception" \
+#   --admin_user="${WP_ADMIN_USER}" \
+#   --admin_password="${WP_ADMIN_PASSWORD}" \
+#   --admin_email="${WP_ADMIN_EMAIL}" \
+#   --allow-root
 
-php8.4-cli /usr/local/bin/wp-cli.phar user create "$WP_USER" "$WP_USER_EMAIL" \
-  --user_pass="$WP_USER_PASSWORD" \
-  --path=/website \
-	--allow-root
+# php8.4-cli /usr/local/bin/wp-cli.phar user create "$WP_USER" "$WP_USER_EMAIL" \
+#   --user_pass="$WP_USER_PASSWORD" \
+#   --path=/website \
+# 	--allow-root
