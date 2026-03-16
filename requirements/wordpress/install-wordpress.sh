195fc9cbcacebb1a26f006cd092e08e523bc46ca
Xr4t0s <nicolastadros.maillol@gmail.com>
2026-03-16 15:10:38 +0100

    update

--- requirements/wordpress/install-wordpress.sh
+++ requirements/wordpress/install-wordpress.sh
@@ -9,3 +9,23 @@ mv wordpress/* /var/www/html/
 chown -R www-data:www-data /var/www/html
 
 sed -i 's|/run/php/php.*sock|9000|' /etc/php/*/fpm/pool.d/www.conf
+
+sudo apt install php8.4-cli -y
+
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+	&& sudo mv wp-cli.phar
+
+php8.4-cli ./wp-cli.phar
+
+php /usr/local/bin/wp-cli.phar core install \
+  --url="https://${DOMAIN_NAME}" \
+  --title="Inception" \
+  --admin_user="${WP_ADMIN_USER}" \
+  --admin_password="${WP_ADMIN_PASSWORD}" \
+  --admin_email="${WP_ADMIN_EMAIL}" \
+  --allow-root
+
+php /usr/local/bin/wp-cli.phar user create "$WP_USER" "$WP_USER_EMAIL" \
+  --user_pass="$WP_USER_PASSWORD" \
+  --path=/website \
+	--allow-root
