ed206ea95518590bcba28e9d309c449f7225e21d
Xr4t0s <nicolastadros.maillol@gmail.com>
2026-03-16 15:56:32 +0100

    update

--- requirements/wordpress/install-wordpress.sh
+++ requirements/wordpress/install-wordpress.sh
@@ -6,7 +6,7 @@ apt-get update && apt-get upgrade -y && apt-get install -y \
 	wget unzip iproute2 procps \
 	php php-fpm php-mysql php-cli php-curl php-gd php-mbstring php-xml php-zip
 
-apt update && apt install php8.4-cli -y 
+
 
 wget https://wordpress.org/latest.zip
 unzip ./latest.zip
