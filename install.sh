619b9081823ee5483e53dbc9e4aa1aa5ea44a343
Xr4t0s <nicolastadros.maillol@gmail.com>
2026-03-16 14:24:57 +0100

    update

--- install.sh
+++ install.sh
@@ -26,6 +26,8 @@ bash ./requirements/nginx/tools/make-certs.sh
 # Adding hostnames to map nitadros.42.fr to localhost
 echo "127.0.0.1 nitadros.42.fr" | sudo tee -a /etc/hosts
 
+mkdir -p /home/nitadros/data/{wp,db}
+
 # Starting containers
 sudo docker compose up -d --build
 
