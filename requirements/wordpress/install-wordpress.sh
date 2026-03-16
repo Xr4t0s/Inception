a6be1dad3d21196314a90e2352629376d8a53867
Xr4t0s <nicolastadros.maillol@gmail.com>
2026-03-16 16:18:24 +0100

    update

--- requirements/wordpress/install-wordpress.sh
+++ requirements/wordpress/install-wordpress.sh
@@ -9,4 +9,6 @@ mkdir -p /var/www/html
 mv wordpress/* /var/www/html/
 chown -R www-data:www-data /var/www/html
 
+cp ./conf/wp-config.php /var/www/html/wp-config.php
+
 sed -i 's|/run/php/php.*sock|9000|' /etc/php/*/fpm/pool.d/www.conf
